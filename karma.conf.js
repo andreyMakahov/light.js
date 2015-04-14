fc1588816c3134086196059df855b2aea7c0f86a
dron <a.makahov@gmail.com>
2015-04-15 02:40:58 +0400

    light.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'min-framework-js.js',
+      'light.js',
       'Tests/**/*.js'
     ],
 
